c981d46591bd149445238febdd0b6d98a092c6b7
omrgrcn <omergercin@gmail.com>
2019-06-18 14:00:36 +0300

    thunk middleware and redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
-    rootReducer
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
