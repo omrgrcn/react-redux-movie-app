dfca4da5ac3b629f64f2d39e50d3d0eae61fe9b2
omrgrcn <omergercin@gmail.com>
2019-07-03 02:28:31 +0300

    MoviesPage redux store connection

--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -1,7 +1,14 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
 
-export default class MoviesPage extends PureComponent {
+class MoviesPage extends Component {
+    static propTypes = {
+        movies: PropTypes.array.isRequired
+    };
+    
     render() {
+        console.log(this.props);
         return (
             <div>
                 <h2>Movies Page</h2>
@@ -9,3 +16,11 @@ export default class MoviesPage extends PureComponent {
         )
     }
 }
+
+const mapStateToProps = ({ movies }) => {
+    return {
+        movies
+    }
+};
+
+export default connect (mapStateToProps)(MoviesPage);
