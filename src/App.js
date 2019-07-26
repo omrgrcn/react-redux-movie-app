6b2075e4416be1fc034eb90e50a6175e7e2de0a1
omrgrcn <omergercin@gmail.com>
2019-07-26 03:18:08 +0300

    nodejs backend installation

--- src/App.js
+++ src/App.js
@@ -19,16 +19,6 @@ class App extends Component {
           <Route path='/movies' component={MoviesPage}></Route>
           <hr/>
           <div>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum odit quae, dolorum amet quis, mollitia dolorem ex, omnis rerum maxime suscipit magni laudantium magnam totam necessitatibus eum excepturi quam quas!
-            Recusandae saepe sint eius officiis a obcaecati hic, animi sed tempora, eos eligendi non voluptatem. Ipsa dolorem, cumque reiciendis unde cum sapiente nemo tempora tenetur obcaecati voluptatem veniam amet aliquid.
-            Blanditiis sed at quo adipisci eum vel architecto reiciendis error ab temporibus, numquam accusantium tenetur vitae ut praesentium voluptatibus porro sit necessitatibus natus, perferendis iusto. Nisi eligendi possimus quidem dicta.
-            Odit impedit qui harum distinctio inventore minima totam dolores nobis nisi sequi? Itaque deleniti doloribus vero numquam dolorum, provident minima reiciendis dicta dolores aperiam minus. Perferendis assumenda sint nostrum nam.
-            Consectetur, libero. Autem cum reprehenderit maxime qui dignissimos porro ratione tempora fugit! Recusandae, ea? Ipsam vitae, labore amet non ex incidunt fugit dolorem doloribus voluptatem eum numquam quas possimus sed!
-            Ea excepturi, voluptates aliquid quasi nihil sapiente, vitae at magnam omnis nostrum dolore iste deleniti alias nobis sit minima quam itaque delectus nam quo eius eum nisi temporibus libero! Autem.
-            Ea voluptates enim aliquam debitis quaerat. Illo unde vel rerum corrupti deleniti, ullam facere. Earum quis commodi voluptas perferendis recusandae cupiditate? Harum eius tempora dolore sint quidem corrupti commodi laboriosam!
-            Maiores commodi saepe beatae quam dolor voluptate laudantium, provident non aperiam? Distinctio quia, quidem rerum officiis excepturi eum, quisquam aliquam reprehenderit porro vel nostrum debitis aperiam exercitationem obcaecati hic amet?
-            Error doloribus sunt rem enim inventore itaque sequi saepe, totam aliquid labore quos asperiores quibusdam temporibus voluptatem! Eveniet eaque expedita adipisci eius voluptatum alias, accusamus deleniti nesciunt aliquam ab quo.
-            Ea nostrum facere velit consequatur impedit? Enim maiores, dolore tenetur magni labore a placeat tempora quasi cupiditate non culpa dicta magnam unde pariatur itaque vero vel beatae voluptates facere. Neque.
           </div>
         </Container>
         <Footer/>
